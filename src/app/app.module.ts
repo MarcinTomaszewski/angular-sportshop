77d7b29a01bf261e995cf75726b4e51cfb5b4588
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2023-02-03 12:14:02 +0100

    Create routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,27 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { StoreModule } from "./store/store.module";
 import { AppComponent } from "./app.component";
-
+import { RouterModule } from "@angular/router";
 import { registerLocaleData } from "@angular/common";
 import localePL from "@angular/common/locales/pl";
+import { StoreComponent } from "./store/store.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+import { CheckoutComponent } from "./store/checkout.component";
+
 registerLocaleData(localePL);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule],
+  imports: [
+    BrowserModule,
+    StoreModule,
+    RouterModule.forRoot([
+      { path: "store", component: StoreComponent },
+      { path: "cart", component: CartDetailComponent },
+      { path: "checkout", component: CheckoutComponent },
+      { path: "**", redirectTo: "/store" },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
