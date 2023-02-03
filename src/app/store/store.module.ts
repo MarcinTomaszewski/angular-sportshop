eba22ce3c1ba643fe36ae072833b5f881ee451c3
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2023-02-03 11:56:00 +0100

    Create cartDetail and checkout components

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,11 +4,19 @@ import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
 import { CartSummaryComponent } from "./cartSummary.component";
+import { CartDetailComponent } from "./cartDetail.component";
+import { CheckoutComponent } from "./checkout.component";
 import { CounterDirective } from "./counter.directive";
 
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [StoreComponent, CartSummaryComponent, CounterDirective],
-  exports: [StoreComponent],
+  declarations: [
+    StoreComponent,
+    CartSummaryComponent,
+    CounterDirective,
+    CartDetailComponent,
+    CheckoutComponent,
+  ],
+  exports: [StoreComponent, CartDetailComponent, CheckoutComponent],
 })
 export class StoreModule {}
